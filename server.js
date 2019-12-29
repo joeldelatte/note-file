94a69c721ce9262dbc486f2f6c1261633725398d
Joel Delatte <joeldelatte@gmail.com>
2019-12-29 14:10:59 -0600

    adds json data response from server displayed in browser

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ app.use(bodyParser.urlencoded({extended: false}))
 
 app.use(bodyParser.json())
 
+require("./routes/api-routes.js")(app); 
 require("./routes/html-routes.js")(app);
-require("./routes/api-routes.js")(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
